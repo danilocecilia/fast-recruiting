04c760d95d2d769167773e9557556feb266078c5
Danilo Cecilia <danilo.cecilia@zenabis.com>
2021-05-11 12:29:22 -0700

    NextJS Auth implementation

--- web/src/pages/_app.js
+++ web/src/pages/_app.js
@@ -1,7 +1,29 @@
+import React from 'react'
 import '../styles/globals.scss'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate } from 'react-query/hydration'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClientRef = React.useRef()
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
+  return (
+    <>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+          </Provider>
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  )
 }
 
 export default MyApp
